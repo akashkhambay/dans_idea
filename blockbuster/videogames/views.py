0fee1422ef7c0628505592a0b1c589582cde4c38
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-26 14:54:53 +0100

    Here is everything guys

---
+++ blockbuster/videogames/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+videogames = [
+    { 'id': 1, 'title': 'Persona 5', 'stock': True },
+    { 'id': 2, 'title': 'Mario Kart', 'stock': False},
+    { 'id': 3, 'title': 'Wii Sports', 'stock': True}
+]
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world... Blockbuster is back. Here are the videogames index")
+
+def about(request):
+    return render(request, '/hello.html')    
