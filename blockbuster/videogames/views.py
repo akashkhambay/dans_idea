1060d7bf6b2f48f2ec587b2a4121191b8796017b
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-26 17:59:32 +0100

    working with games showing on page

--- blockbuster/videogames/views.py
+++ blockbuster/videogames/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import tbc
+from .models import Game
 
 # videogames = [
 #     { 'id': 1, 'title': 'Persona 5', 'stock': True },
@@ -11,9 +11,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    # get the info for each game.
-    # pass that as context
-    return render(request, '', context)
+    games = Game.objects.all()
+    context = {"games": games}
+    return render(request, 'videogames/game-store.html', context)
 
 def about(request):
     return render(request, 'videogames/about.html')    
