2a5f3493ae44cda9b82725c0c2661052262093d7
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-26 17:09:22 +0100

    Working db

--- blockbuster/users/urls.py
+++ blockbuster/users/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.Loginview.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_views(template_name='users/logout.html'), name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
--- blockbuster/videogames/models.py
+++ blockbuster/videogames/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Game (models.Model):
+    name = models.CharField(max_length=600)
+    rating = models.PositiveIntegerField(range(1,10))
+    in_stock = models.BooleanField(True)
